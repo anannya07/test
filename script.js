90f03a59e5d7e97b801d7f595b007b750ceba814
Anannya Guchait <131895159+anannya07@users.noreply.github.com>
2023-07-14 13:32:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ function signOut()
      $(".data").css("display","none");
   });
 }
+npm install google-auth-library --save
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client();
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: 80499037976-ltl3dh9jodcr59f6af38947bvlmb9ma1.apps.googleusercontent.com,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+}
+verify().catch(console.error);
