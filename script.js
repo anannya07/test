abcb335088314bf7c8feb891b3b3296e720a4082
Anannya Guchait <131895159+anannya07@users.noreply.github.com>
2023-07-14 12:11:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function onSignIn(googleuser)
 {
   var profile= googleUser.getBasicProfile();
+  $(".g_id_signin").css("display","none");
+  $(".data").css("display","block");
+  $("#pic") .attr('src',profile.getImageUrl());
+  $("#email").text("profile.getEmail());
+ }
   
