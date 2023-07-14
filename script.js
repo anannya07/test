ae4df2b8b40a996874632f749cfc63d3a5b195b3
Anannya Guchait <131895159+anannya07@users.noreply.github.com>
2023-07-14 12:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function onSignIn(googleuser)
 {
   var profile= googleUser.getBasicProfile();
-  $(".g_id_signin").css("display","none");
+  $(".signinButton").css("display","none");
   $(".data").css("display","block");
   $("#pic") .attr('src',profile.getImageUrl());
   $("#email").text("profile.getEmail());
  }
   
-  $('#signinButton').click(function() {
+  $('.signinButton').click(function() {
     // signInCallback defined in step 6.
     auth2.grantOfflineAccess().then(signInCallback);
   });
